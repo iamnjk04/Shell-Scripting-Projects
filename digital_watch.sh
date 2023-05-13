432c5145902bbef6be958d3b4e164a6e2bcb14dd
iamnjk04 <iamnjk04@gmail.com>
2023-05-13 21:33:12 +0545

    Updated the digital watch

--- digital_watch.sh
+++ digital_watch.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+# Pzrogram to show a continuous operatig digital watch
+
+#Colour Code for RGB
 Red=$'\e[1;31m'
 Green=$'\e[1;32m'
 Blue=$'\e[1;34m'
+
+
+<<We use While loop
+to make an infinite loop
+and at every ieration:
+1. Clear terminal
+2. Print time using formating date
+3. Sleep for 1 sec 
+We
 
 
 while true
